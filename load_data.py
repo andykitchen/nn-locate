7f4489733ce04158c1e5318dd888e919df036358
Andy Kitchen <kitchen.andy+git@gmail.com>
2016-07-22 14:58:35 +1000

    setup validation data

--- load_data.py
+++ load_data.py
@@ -13,9 +13,14 @@ def glob_loader(globs):
     labels = [label for image, label in images_with_label]
     return images, labels
 
-def load_data():
-    images, labels = glob_loader(['images/background_*.jpg', 'images/shark_*.jpg'])
+def load_data_globs(globs):
+    images, labels = glob_loader(globs)
     images_small = [cv2.resize(image, (64, 64)) for image in images]
     X = np.stack(images_small).astype(np.float32)
     Y = np.array(labels)
-    return (X, Y), (X, Y)
+    return (X, Y)
+
+def load_data():
+    X_train, Y_train = load_data_globs(['images/background_*.jpg', 'images/shark_*.jpg'])
+    X_validation, Y_validation = load_data_globs(['images_validation/background_*.jpg', 'images_validation/shark_*.jpg'])
+    return (X_train, Y_train), (X_validation, Y_validation)
